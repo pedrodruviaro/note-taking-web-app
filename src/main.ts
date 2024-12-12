508be43a94c80736a833fa800b4a5dfeaad80147
pedrodruviaro <pedrodruviaro@gmail.com>
2024-12-12 14:23:56 -0300

    feat: add tailwind and primevue

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import '@/assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import PrimeVue from 'primevue/config'
 import App from '@/App.vue'
 import router from '@/router'
 
@@ -10,5 +11,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: 'none',
+})
 
 app.mount('#app')
